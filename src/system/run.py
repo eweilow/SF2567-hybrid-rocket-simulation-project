c708556e6675f82f6caf104af55611b798f98408
eweilow <eweilow@digitalkoppar.se>
2020-11-11 10:41:33 +0100

    Some more refactoring

--- src/system/run.py
+++ src/system/run.py
@@ -3,7 +3,7 @@ import numpy as np
 from solve.ode import makeODE
 from scipy.integrate import solve_ivp
 
-from solve.dependencies import recurseModelDependencies
+from solve.dependencies import recurseModelDependencies, applyDerivedVariablesToResult
 from solve.states import applyModelStates, collectModelStates
 
 models, system, events, initialState = makeODE()
@@ -15,49 +15,21 @@ sol = solve_ivp(
   [0, maximumSolveTime], 
   initialState, 
   'LSODA', 
-  t_eval=np.linspace(0, maximumSolveTime, 5000), 
+  t_eval=np.linspace(0, maximumSolveTime, 500), 
   dense_output=False, 
   events=events,
-  max_step=1e-1
+  #max_step=1e-1
 )
 
-sol.t = sol.t[1:]
-sol.y = sol.y[:,1:]
-states = sol.y
+applyDerivedVariablesToResult(sol.t, sol.y, models)
 
-for key in models:
-  models[key]["derivedResult"] = np.zeros((len(models[key]["derived"]), len(sol.t)))
-
-# Recover derived variables
-for i in range(len(sol.t)):
-  y = sol.y[:,i]
-  print(sol.t[i])
-  applyModelStates(models, y)
-
-  # Set up derived variables
-  visited = []
-  for key in models:
-    visited = recurseModelDependencies(sol.t[i], models[key], 0, models, visited)
-
-  for key in models:
-    models[key]["derivedResult"][:, i] = models[key]["derived"]
-
-
-applyModelStates(models, sol.y)
 # Recover state for different parts
 for key in models:
   models[key]["state"] = np.dot(models[key]["invMatrix"], sol.y)
 
-
 with open('/data/simulation.npy', 'wb') as f:
   np.save(f, sol.t)
   
-  np.save(f, models["tank"]["state"])
-  np.save(f, models["tank"]["derivedResult"])
-  np.save(f, models["injector"]["state"])
-  np.save(f, models["injector"]["derivedResult"])
-  np.save(f, models["combustion"]["state"])
-  np.save(f, models["combustion"]["derivedResult"])
-  np.save(f, models["nozzle"]["state"])
-  np.save(f, models["nozzle"]["derivedResult"])
-
+  for key in models:
+    np.save(f, models[key]["state"])
+    np.save(f, models[key]["derivedResult"])
--- src/system/solve/dependencies.py
+++ src/system/solve/dependencies.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 """
 Compute derived variables for all the models in the order that is defined by each model
 in the function "derivedVariablesDependsOn".
@@ -15,3 +18,26 @@ def recurseModelDependencies(t, obj, currentDepth, models, visited):
   obj["derived"] = obj["model"].computeDerivedVariables(t, obj["state"], models)
 
   return visited
+
+
+from solve.states import applyModelStates, collectModelStates
+
+def applyDerivedVariablesToResult(t, y, models):
+  
+  for key in models:
+    models[key]["derivedResult"] = np.zeros((len(models[key]["derived"]), len(t)))
+
+    # Recover derived variables
+  for i in range(len(t)):
+    print(t[i])
+    applyModelStates(models, y[:,i])
+
+    # Set up derived variables
+    visited = []
+    for key in models:
+      visited = recurseModelDependencies(t[i], models[key], 0, models, visited)
+
+    for key in models:
+      models[key]["derivedResult"][:, i] = models[key]["derived"]
+      
+  applyModelStates(models, y)
