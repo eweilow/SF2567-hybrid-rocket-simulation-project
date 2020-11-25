1514883355cdcff14963e141aca68b05371498f2
eweilow <eweilow@digitalkoppar.se>
2020-11-25 11:52:55 +0100

    Clean up runner for DAE solver start

--- src/system/runSingle.py
+++ src/system/runSingle.py
@@ -12,60 +12,58 @@ import assumptions
 from solve.dependencies import recurseModelDependencies, applyDerivedVariablesToResult
 from solve.states import applyModelStates, collectModelStates
 
+def solver(
+  sys,
+  y0,
+  fromTime,
+  toTime,
+  outputTimestep,
+  events = None
+):
+  sol = solve_ivp(
+    sys, 
+    [fromTime, toTime], 
+    y0, 
+    'LSODA', 
+    t_eval=np.arange(fromTime, toTime, outputTimestep), 
+    dense_output=False, 
+    events=events
+  )
+
+  return sol.t, sol.y, sol.t[-1]
+
+def recoverModelState(t, y, models):
+  applyDerivedVariablesToResult(t, y, models)
+  # Recover state for different parts
+  for key in models:
+    models[key]["state"] = np.dot(models[key]["invMatrix"], y)
+
+    if not models[key]["simplifiedInit"] is None:
+      mask, args = models[key]["simplifiedInit"]
+      for i in range(len(mask)):
+        if mask[i] is None:
+          models[key]["state"][i,:] = None
 
 start = time.time()
 
-models, system, events, initialState, hit_ground_event = makeODE()
 maximumSolveTime = 40
 options.printTime = False
 
-sol = solve_ivp(
-  system, 
-  [0, maximumSolveTime], 
-  initialState, 
-  'LSODA', 
-  t_eval=np.linspace(0, maximumSolveTime, 500), 
-  dense_output=False, 
-  events=events,
-  #max_step=1e-1
-)
+models, system, events, initialState, hit_ground_event = makeODE()
+t, y, tend = solver(system, initialState, 0, maximumSolveTime, 0.05, events)
+recoverModelState(t, y, models)
 
-applyDerivedVariablesToResult(sol.t, sol.y, models)
-# Recover state for different parts
-for key in models:
-  models[key]["state"] = np.dot(models[key]["invMatrix"], sol.y)
-
-models2, system2, events2, initialState2, hit_ground_event2 = makeODE(simplified = True, timeHistory=sol.t, previousModels=models)
-
-sol2 = solve_ivp(
-  system2, 
-  [sol.t[-1], sol.t[-1] + 120], 
-  sol.y[:,-1], 
-  'LSODA', 
-  t_eval=np.linspace(sol.t[-1], sol.t[-1] + 120, 100), 
-  dense_output=False, 
-  events=(hit_ground_event2)
-  #max_step=1e-1
-)
-applyDerivedVariablesToResult(sol2.t, sol2.y, models2)
-
-# Recover state for different parts
-for key in models2:
-  models2[key]["state"] = np.dot(models2[key]["invMatrix"], sol2.y)
-
-  if not models[key]["simplifiedInit"] is None:
-    mask, args = models[key]["simplifiedInit"]
-    for i in range(len(mask)):
-      if mask[i] is None:
-        models2[key]["state"][i,:] = None
-
+models2, system2, events2, initialState2, hit_ground_event2 = makeODE(simplified = True, timeHistory=t, previousModels=models)
+t2, y2, tend2 = solver(system2, y[:,-1], tend, tend + 240, 0.1, events=(hit_ground_event2))
+applyDerivedVariablesToResult(t2, y2, models2)
+recoverModelState(t2, y2, models2)
 
 end = time.time()
 print("Running simulation took {:}".format(end - start))
 
 with open("/data/simulation.npy", 'wb') as f:
-  np.save(f, [0, sol.t[-1], sol2.t[-2]])
-  np.save(f, np.concatenate((sol.t, sol2.t)))
+  np.save(f, [0, t[-1], t2[-2]])
+  np.save(f, np.concatenate((t, t2)))
   for key in models:
     np.save(f, np.concatenate((models[key]["state"], models2[key]["state"]), axis=1))
     np.save(f, np.concatenate((models[key]["derivedResult"], models2[key]["derivedResult"]), axis=1))
