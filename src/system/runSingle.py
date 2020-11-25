80ca87642a901fabc4fc797150462a4e15688d10
eweilow <eweilow@digitalkoppar.se>
2020-11-25 14:23:46 +0100

    WORKING DAE RUN

--- src/system/runSingle.py
+++ src/system/runSingle.py
@@ -18,18 +18,92 @@ def solver(
   fromTime,
   toTime,
   outputTimestep,
-  events = None
+  events = None,
+  useDae = False
 ):
+  times = np.arange(fromTime, toTime, outputTimestep)
+
+  # sys is of the form def system(t, y)
+  if useDae:
+    from scikits.odes import dae
+    def addafunc(t, y, ml, mu, p, nrowp):
+      return p - np.eye(nrowp)
+      
+    def residual(t, y, ydot, result):
+      try:
+        derivative = sys(t, y)
+      except Exception:
+        return 1
+
+      N = np.size(derivative)
+      for i in range(N):
+        result[i] = ydot[i] - derivative[0, i]
+      
+      # print(t, np.linalg.norm(result))
+      # print(", ".join(["{:9.2e}".format(result[i]) for i in range(N)]))
+      # print(", ".join(["{:9.2e}".format(ydot[i]) for i in range(N)]))
+      # print(", ".join(["{:9.2e}".format(derivative[0, i]) for i in range(N)]))
+      
+      return 0
+    
+    daeSolver = "lsodi"
+
+    rtol = 1e-3
+    atol = 1e-3
+
+    if daeSolver == "ida":
+      solver = dae('ida', residual, 
+        exclude_algvar_from_error=True,
+        adda_func=addafunc,
+        algebraic_vars_idx=None,
+        atol=atol,
+        rtol=rtol,
+        max_steps=500000
+      )
+
+      yp0 = sys(fromTime, y0)
+      solution = solver.solve(times, y0, yp0[0,:])
+
+      if not solution.flag == 1:
+        raise Exception("Flag is not 0 (it is " + str(solution.flag) + ")")
+
+      t = np.array(solution.values.t)
+      v = np.array(solution.values.y)
+      return np.transpose(t), np.transpose(v), t[-1]
+    elif daeSolver == "lsodi":
+      solver = dae('lsodi', residual, 
+        method="adams",
+        order=12,
+        exclude_algvar_from_error=True,
+        adda_func=addafunc,
+        algebraic_vars_idx=None,
+        atol=atol,
+        rtol=rtol,
+        max_steps=500000
+      )
+
+      yp0 = sys(fromTime, y0)
+      solution = solver.solve(times, y0, yp0[0,:])
+
+      flag, t, v, *rest = solution
+      return np.transpose(t), np.transpose(v), t[-1]
+    else:
+      raise Exception("Unknown solver")
+    # t, y = values
+    #return t, y, t[-1]
+
   sol = solve_ivp(
     sys, 
     [fromTime, toTime], 
     y0, 
     'LSODA', 
-    t_eval=np.arange(fromTime, toTime, outputTimestep), 
+    t_eval=times, 
     dense_output=False, 
     events=events
   )
-
+  
+  print(np.shape(sol.t))
+  print(np.shape(sol.y))
   return sol.t, sol.y, sol.t[-1]
 
 def recoverModelState(t, y, models):
@@ -46,15 +120,16 @@ def recoverModelState(t, y, models):
 
 start = time.time()
 
-maximumSolveTime = 40
+maximumSolveTime = 10
 options.printTime = False
 
 models, system, events, initialState, hit_ground_event = makeODE()
-t, y, tend = solver(system, initialState, 0, maximumSolveTime, 0.05, events)
+t, y, tend = solver(system, initialState, 0, maximumSolveTime, 0.05, events, useDae=True)
+print(tend)
 recoverModelState(t, y, models)
 
 models2, system2, events2, initialState2, hit_ground_event2 = makeODE(simplified = True, timeHistory=t, previousModels=models)
-t2, y2, tend2 = solver(system2, y[:,-1], tend, tend + 240, 0.1, events=(hit_ground_event2))
+t2, y2, tend2 = solver(system2, y[:,-1], tend, tend + 240, 0.1, events=(hit_ground_event2), useDae=False)
 applyDerivedVariablesToResult(t2, y2, models2)
 recoverModelState(t2, y2, models2)
 
